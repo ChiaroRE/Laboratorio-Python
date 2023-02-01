17cd9d82937d4cedafe021855346444e4bca12f4
Replit user <>
2023-02-01 17:45:09 +0000

    Esame/esame.py: Modifica metodo compute_..._difference per efficentamento, prima versione storata in file Esame/prima_versione.py

--- Esame/esame.py
+++ Esame/esame.py
@@ -33,40 +33,25 @@ class CSVTimeSeriesFile():
         return list
 
 def compute_daily_max_difference(time_series):
-  first_element = time_series[0]
-  last_element = time_series[-1]
+    first_element = time_series[0]
+    last_element = time_series[-1]
   
-  day_start_epoch = first_element[0] - (first_element[0] % 86400)
-  day_end_epoch = day_start_epoch + 86399
+    day_start_epoch = first_element[0] - (first_element[0] % 86400)
+    day_end_epoch = day_start_epoch + 86399
   
-  day_temp_1 = []
-  for item in time_series:
-    if item[0] < day_end_epoch and item[0] >= day_start_epoch:
-      day_temp_1.append(item[1])
-  diff = []
-  if len(day_temp_1) <= 1:
-    differenza_massima_giorno = None
-  else:
-    differenza_massima_giorno = max(day_temp_1) - min(day_temp_1)
-  diff.append(differenza_massima_giorno)
-  
-  day_start_epoch = day_end_epoch + 1
-  day_end_epoch = day_end_epoch + 86399
-  
-  while(day_start_epoch < last_element[0]):
-    day_temp = []
+    diff = []
+    temp = []
+    
     for item in time_series:
-      if item[0] < day_end_epoch and item[0] >= day_start_epoch:
-        day_temp.append(item[1])
-    if(len(day_temp) <= 1):
-      differenza_massima_giorno = None
-    else:
-      differenza_massima_giorno = max(day_temp) - min(day_temp)
-    diff.append(differenza_massima_giorno)
-    day_start_epoch = day_end_epoch + 1
-    day_end_epoch = day_end_epoch + 86399
+        if item[0] <= day_end_epoch:
+            temp.append(item[1])
+        if item[0] > day_end_epoch or item[0] == last_element[0]:
+            diff.append(max(temp) - min(temp))
+            day_end_epoch = day_end_epoch + 86399
+            temp = [item[1]]
+            
 
-  return diff
+    return diff
   
   
 
@@ -74,5 +59,7 @@ time_series_file = CSVTimeSeriesFile('data.csv')
 time_series = time_series_file.get_data()
 
 diff = (compute_daily_max_difference(time_series))
+print(diff)
 print(len(diff))
+
 
